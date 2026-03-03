18cc2d21738c8873a669a72c4995fbb67c0413bc
Senanyomi <senanyoming@gmail.com>
2026-03-03 11:29:46 +0000

    Update API base URL in axios configuration

--- interface/src/api/axios.ts
+++ interface/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://facility-booking-9u2i.onrender.com/api',
+  baseURL: 'https://group15-campusfacilitybookingsystem-px.onrender.com/api',
   headers: { 'Content-Type': 'application/json', Accept: 'application/json' },
 });
 
